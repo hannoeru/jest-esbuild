68531e74587b751081be7a360fe9fa4128c0b66b
hanlee <me@hanlee.co>
2021-03-07 20:26:15 +0900

    fix: add default loader

--- src/options.ts
+++ src/options.ts
@@ -3,6 +3,7 @@ import { UserOptions } from './type'
 
 export function resolveOptions(userOptions: UserOptions): TransformOptions {
   return {
+    loader: userOptions.loader || 'ts',
     format: userOptions.format || 'cjs',
     target: userOptions.target || 'es2018',
     ...(userOptions.jsxFactory ? { jsxFactory: userOptions.jsxFactory } : {}),
