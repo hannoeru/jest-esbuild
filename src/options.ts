58092636a12f686798c730d491e8ee76db93147d
hanlee <me@hanlee.co>
2021-04-22 22:20:00 +0900

    feat: allow pass all options to esbuild, close #2

--- src/options.ts
+++ src/options.ts
@@ -2,11 +2,10 @@ import { UserOptions, ResolvedOptions } from './type'
 
 export function resolveOptions(userOptions: UserOptions): ResolvedOptions {
   return {
-    loader: userOptions.loader || 'ts',
-    format: userOptions.format || 'cjs',
-    target: userOptions.target || 'es2018',
-    ...(userOptions.jsxFactory ? { jsxFactory: userOptions.jsxFactory } : {}),
-    ...(userOptions.jsxFragment ? { jsxFragment: userOptions.jsxFragment } : {}),
-    sourcemap: userOptions.sourcemap ?? true,
+    loader: 'ts',
+    format: 'cjs',
+    target: 'es2018',
+    sourcemap: true,
+    ...userOptions,
   }
 }
